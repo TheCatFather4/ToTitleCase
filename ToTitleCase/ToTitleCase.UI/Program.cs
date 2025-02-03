2107726ab64b5349be7d45d68939ddaf8754eb0a
TheCatFather4 <marcelloja.csharp@gmail.com>
2025-02-03 00:28:38 -0500

    Added example of a string extension method

---
+++ ToTitleCase/ToTitleCase.UI/Program.cs
@@ -0,0 +1,21 @@
+﻿using static ToTitleCase.UI.StringExtensions;
+
+string input;
+
+do
+{
+    Console.Write("Enter a sentence: ");
+    input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("This field is required");
+    }
+    else
+    {
+        break;
+    }
+} while (true);
+
+string result = input.ToTitleCase();
+
+Console.WriteLine($"\n\"{result}\" is the input in title case.");
---
+++ ToTitleCase/ToTitleCase.UI/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToTitleCase.UI
+{
+    public static class StringExtensions
+    {
+        public static string ToTitleCase(this string s)
+        {
+            string[] words = s.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                words[i] = char.ToUpper(word[0]) + word.Substring(1).ToLower();
+            }
+
+            return string.Join(' ', words);
+        }
+    }
+}
